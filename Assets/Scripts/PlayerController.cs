7ec10e26a368a7894a6695312caf0370fbd56ff4
Christina Phillips <christina.phillips9838@gmail.com>
2025-05-29 00:49:00 -0400

    Finished implmenting object collisons section

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables related to player character movement
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2D;
+    Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);    
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;  
-        transform.position = position;  
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
     }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2D.position + move * 3.0f * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+    }
+        
     
 }
