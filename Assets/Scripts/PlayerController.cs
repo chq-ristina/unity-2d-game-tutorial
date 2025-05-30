be6238b9b9a3b805d068b4f5cc67e169b79ececb
Christina Phillips <christina.phillips9838@gmail.com>
2025-05-29 23:55:33 -0400

    Added colliders to tilemap and introduced health system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2D;
     Vector2 move;
+    public float speed = 3.0f;
+    
+    //Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //Start is called before the first frame update
@@ -14,6 +19,7 @@ public class PlayerController : MonoBehaviour
     {
         MoveAction.Enable();
         rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -22,12 +28,17 @@ public class PlayerController : MonoBehaviour
         move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
     }
-
+    
+    //FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2D.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2D.position + move * speed * Time.deltaTime;
         rigidbody2D.MovePosition(position);
     }
-        
-    
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
