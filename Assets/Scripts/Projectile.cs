205efbf035d8215d9c2993886f4ebd10535319e9
Christina Phillips <christina.phillips9838@gmail.com>
2025-06-28 21:04:50 -0400

    Projectile affecting enemies

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,26 +4,39 @@ using UnityEngine.InputSystem;
 public class Projectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    // Awake is called when the Projectile GameObject is instantiated
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if(transform.position.magnitude > 100.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
     public void Launch(Vector2 direction, float force)
     {
-        //The function calls AddForce on the projectile’s Rigidbody component; the calculation for this here is
-        //the direction variable multiplied by the force variable.
-        // When the force is applied to the Rigidbody component, the physics engine will apply that force and direction
-        // to move the Projectile GameObject every frame.
         rigidbody2d.AddForce(direction * force);
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Projectile collision with " + other.gameObject);
+        EnemyController enemy = other.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            enemy.Fix();
+        }
+
+
         Destroy(gameObject);
     }
-    
+
 }
